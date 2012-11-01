d3bfd44345c7837e60d934e45d92bb4f94e7f915
Ghica Alexandru <alec.ghica@eaudeweb.ro>
2012-11-01 15:58:51 +0200

    Add 'Taskman project' in front of project title in header, refs #9891

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,2 +1,16 @@
 // Put your javascript here and it will automatically be loaded on each page.
 
+// Change project title in header
+function setupProjectTitle() {
+    var project_title = $('#header h1').html();
+    var pathname = window.location.pathname;
+
+    if  (pathname.match('/projects/') || pathname.match('/issues/')) {
+        $('#header h1').html('Taskman project: ' + project_title);
+    }
+}
+
+// Load custom JavaScript
+jQuery(document).ready(function() {
+    setupProjectTitle();
+});
